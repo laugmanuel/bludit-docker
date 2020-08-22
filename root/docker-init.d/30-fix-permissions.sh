9dd5df484b8686d8bab7191c7e36d1b3150150f1
Manuel Laug <laugmanuel@gmail.com>
2020-08-22 21:19:59 +0200

    Fix permission script

--- root/docker-init.d/30-fix-permissions.sh
+++ root/docker-init.d/30-fix-permissions.sh
@@ -7,11 +7,9 @@ chown -R nginx:nginx ${TARGET_PATH}/* ${TARGET_PATH}/.*
 
 echo "  - Fixing file mode ..."
 find ${TARGET_PATH}/ -type f | xargs chmod 664
-find ${TARGET_PATH}/bin -type f | xargs chmod 775
 
 echo "  - Fixing directory mode ..."
 find ${TARGET_PATH}/ -type d | xargs chmod 775
-find ${TARGET_PATH}/ -type d | xargs chmod +s
 
 echo "  - Setting umask ..."
 (
